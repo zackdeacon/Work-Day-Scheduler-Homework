c62d38269c867febbf19e11c670e6427fb207747
Zachary Deacon <zackdeacon347@gmail.com>
2020-06-18 18:24:51 -0700

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+//TODO: create timeblocks and current date divs that auto populate when the calendar is opened up
+    //TODO: Use time stamp code found in window (console) to achieve current date and time 
+    //date.now (potential) moment.js for library
+
+//color code 1 hour time blocks. use background color 
+    //green = future hours
+    //blue = current hour
+    //red = past hours 
+
+//.sibling method to access sibling in click event listener. this.sibling(input type) grab value and id to send to local storage.
+//when retrieve from local storage can set text content to value 
